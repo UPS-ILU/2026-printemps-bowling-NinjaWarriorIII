c5c089f14ffa0811ee5559d23748e2b653ce09a1
Matthieu <staubmatthieu@gmail.com>
2026-04-16 18:44:56 +0200

    ITER_2

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -19,4 +19,11 @@ public class GameTest {
 		assertEquals(0, game.score());
 	}
 	
+	@Test
+	void test_aucune_quille() {
+		for(int i = 0; i < 20; i++) {
+			game.roll(0);
+		}
+		assertEquals(0, game.score());
+	}
 }
